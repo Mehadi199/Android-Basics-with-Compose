70a8cf128f3517816d147a24a37c1d41f87ff5a8
Md Mehadi <95706997+Mehadi199@users.noreply.github.com>
2022-08-08 19:55:54 +0600

    Create p.solve16 & 17.kt

---
+++ Android-Basics-in-Kotlin/220725AndrioidBasicsinCompose/Unit1/useing variable in kotlin/p.solve16 & 17.kt
@@ -0,0 +1,25 @@
+fun main() {
+    var count = 90
+    println("You have $count unread messages.")
+    count = count + 5
+    println("You have $count read messages.")
+}
+
+//output
+
+// You have 90 unread messages.
+// You have 95 read messages.
+
+fun main() {
+    var count = 90
+    println("You have $count unread messages.")
+    count = count - 15
+    println("You have $count read messages.")
+}
+
+//output
+
+// You have 90 unread messages.
+// You have 75 read messages.
+
+//////////////////
