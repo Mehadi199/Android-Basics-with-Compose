aff24a84e5c9bb76f6f57fb62e3dd6fdcd8faf5c
Md Mehadi <95706997+Mehadi199@users.noreply.github.com>
2022-08-08 20:10:52 +0600

    Create p.solve20 & 21.kt

---
+++ Android-Basics-in-Kotlin/220725AndrioidBasicsinCompose/Unit1/useing variable in kotlin/p.solve20 & 21.kt
@@ -0,0 +1,51 @@
+fun main() {
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double = 0.0
+    println("$totalTripLength miles left to destination")
+}
+
+//output
+// 0.0 miles left to destination
+
+///////
+
+fun main() {
+    val trip1 = 3.20
+    val trip2 = 4.10
+    val trip3 = 1.72
+    val totalTripLength = 0.0
+    println("$totalTripLength miles left to destination")
+}
+
+//output
+// 0.0 miles left to destination
+
+//////
+
+fun main() {
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double = trip1+trip2+trip3
+    println("$totalTripLength miles left to destination")
+}
+
+//output
+// 9.02 miles left to destination
+
+///////////
+
+fun main() {
+    val trip1 = 3.20
+    val trip2 = 4.10
+    val trip3 = 1.72
+    val totalTripLength = trip1+trip2+trip3
+    println("$totalTripLength miles left to destination")
+}
+
+//output
+// 9.02 miles left to destination
+
+////////////////////////////////////
