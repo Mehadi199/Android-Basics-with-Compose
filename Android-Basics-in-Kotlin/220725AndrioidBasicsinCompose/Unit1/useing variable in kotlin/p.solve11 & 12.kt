3f052acb4ffc7b6dd655f8d42fbe1ade287e3599
Md Mehadi <95706997+Mehadi199@users.noreply.github.com>
2022-08-08 19:41:47 +0600

    Create p.solve11 & 12.kt

---
+++ Android-Basics-in-Kotlin/220725AndrioidBasicsinCompose/Unit1/useing variable in kotlin/p.solve11 & 12.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var cartTotal = 0
+    cartTotal = 20
+    println("Total: $cartTotal")
+}
+
+// output
+// Total: 20
+/////////////////////
+
+
+fun main() {
+    var cartTotal = 0
+    println("Total: $cartTotal")
+
+    cartTotal = 20
+    println("Total: $cartTotal")
+}
+
+//output
+
+// Total: 0
+// Total: 20
