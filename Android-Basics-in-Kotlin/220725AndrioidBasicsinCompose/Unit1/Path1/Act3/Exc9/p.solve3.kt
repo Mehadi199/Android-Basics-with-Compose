834c05914e7a3d41a3bf8a7414c8ff09f55e0e38
Md Mehadi <95706997+Mehadi199@users.noreply.github.com>
2022-08-08 19:00:07 +0600

    Create p.solve3.kt

---
+++ Android-Basics-in-Kotlin/220725AndrioidBasicsinCompose/Unit1/Path1/Act3/Exc9/p.solve3.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+}
+
+
+//output
+
+// Monday
+// Tuesday
+// Wednesday
+// Thursday
+// Friday
