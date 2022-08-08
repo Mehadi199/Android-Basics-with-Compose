b47d361f2538e9f74967f480f103ae2d0b295351
Md Mehadi <95706997+Mehadi199@users.noreply.github.com>
2022-08-08 19:07:31 +0600

    Create p.solve4 & 5.kt

---
+++ Android-Basics-in-Kotlin/220725AndrioidBasicsinCompose/Unit1/Path1/Act3/Exc9/p.solve4 & 5.kt
@@ -0,0 +1,16 @@
+fun main() {
+    println("Tomorrow is rainy")
+}
+
+//output
+// Tomorrow is rainy
+
+
+fun main() {
+    println("There is a chance of snow")
+}
+
+// output
+// There is a chance of snow
+
+
