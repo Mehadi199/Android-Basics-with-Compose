9413a7992c4259bc68a7f547faa60f31e85525b4
Md Mehadi <95706997+Mehadi199@users.noreply.github.com>
2022-08-08 19:10:36 +0600

    Create p.solve6 & 7.kt

---
+++ Android-Basics-in-Kotlin/220725AndrioidBasicsinCompose/Unit1/Path1/Act3/Exc9/p.solve6 & 7.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Cloudy"); println("Partly Cloudy"); println("Windy")
+}
+
+//output
+// Cloudy 
+// Partly Cloudy
+// Windy
+
+
+fun main() {
+    println("How's the weather today?")
+}
+
+//output
+// How's the weather today?
+
+///////
